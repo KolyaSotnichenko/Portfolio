b05ef1ae8ba671c2ea38ebb692f6f891e0f62844
Kolya Sotnichenko <kolya.sotnichenko0811@gmail.com>
2021-08-31 11:39:00 +0300

    Add new banners

--- src/containers/greeting/BannerImg.js
+++ src/containers/greeting/BannerImg.js
@@ -1,25 +1,25 @@
 import lottie from 'lottie-web';
-import React, {useRef,useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 // import { Component } from "react";
 // import banner from "./greeting-01.svg"
 
 //for animated svgs use json file
-export default function BannerImg(){
-    const container = useRef(null)
-    useEffect(() => {
-      lottie.loadAnimation({
-        container: container.current,
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        animationData: require('./banner.json')
-      })
-    }, [])
-    return (
-      <div className="App">
-        <div className="container" ref={container}></div>
-      </div>
-    );
+export default function BannerImg() {
+  const container = useRef(null)
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./banner5.json')
+    })
+  }, [])
+  return (
+    <div className="App">
+      <div className="container" ref={container}></div>
+    </div>
+  );
 }
 
 // //for static svgs
