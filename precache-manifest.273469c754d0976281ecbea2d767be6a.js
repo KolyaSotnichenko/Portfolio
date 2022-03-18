1267a588192c90035cb8c9bb2cf1734d845afe18
Kolya Sotnichenko <user.email>
2022-03-18 15:11:36 +0200

    Updates

---
+++ precache-manifest.273469c754d0976281ecbea2d767be6a.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f8f66ca1d6ccd91a26f0aa2ea5fb97",
+    "url": "/KolyaSotnichenko/Portfolio/index.html"
+  },
+  {
+    "revision": "c7a3f9569c8bc9f50a74",
+    "url": "/KolyaSotnichenko/Portfolio/static/css/main.a65cb71c.chunk.css"
+  },
+  {
+    "revision": "b8dcdf7b38a95915d5a0",
+    "url": "/KolyaSotnichenko/Portfolio/static/js/2.0cef7a75.chunk.js"
+  },
+  {
+    "revision": "c7a3f9569c8bc9f50a74",
+    "url": "/KolyaSotnichenko/Portfolio/static/js/main.8a7752f4.chunk.js"
+  },
+  {
+    "revision": "f98138b5d6b85fe0c053",
+    "url": "/KolyaSotnichenko/Portfolio/static/js/runtime-main.ab266d79.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "b8349593342654d8ca5686e6cb73f13b",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/Code.b8349593.ttf"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c0c6a34992de89a9c84f0d2e6550c05e",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/Relu-Activation-Function.c0c6a349.gif"
+  },
+  {
+    "revision": "5eae86485d4f6947c36071cbd15b364a",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/acti.5eae8648.gif"
+  },
+  {
+    "revision": "5c232fa18ee1828e7bd6ff8de497f4e2",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/activation-functions.5c232fa1.gif"
+  },
+  {
+    "revision": "1a58a3f34fa43db27bc8b56cc191614b",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/coursera-01.1a58a3f3.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "e98ff6734ccf75ecfd3004c409872656",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/logo2.e98ff673.png"
+  },
+  {
+    "revision": "5aa6bdcc131e211e4b7f4aa77934ca68",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/neural.5aa6bdcc.gif"
+  },
+  {
+    "revision": "4f42a4cb6130711885ca92e07912dc0d",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/news.4f42a4cb.gif"
+  },
+  {
+    "revision": "858dbff1d8625a63aa0c55e5df5612b1",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/project-02.858dbff1.jpg"
+  },
+  {
+    "revision": "1765a870735a4720590571ef2b9c67d9",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/prometheus.1765a870.png"
+  },
+  {
+    "revision": "97948ca733915eb236871a1b422e3a2b",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/voice.97948ca7.gif"
+  },
+  {
+    "revision": "9331f9728bff50b3d6b330f7d5d6bc51",
+    "url": "/KolyaSotnichenko/Portfolio/static/media/web.9331f972.svg"
+  }
+]);
